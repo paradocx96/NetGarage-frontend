2d39fc8b8a8a43a2a1b9e46df3228c5a99f619d8
paradocx96 <adilanga@gmail.com>
2021-07-30 16:50:02 +0530

    Route - Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Redirect, Switch, Route} from 'react-router-dom';
 import './App.css';
 
+//HOMEPAGE
 import Homepage from "./pages/Homepage";
 
+
+//LOGIN
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+
+
 function App() {
-  return (
-    <div>
-      <Homepage/>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    {/* HOMEPAGE */}
+                    <Route exact path="/" component={Homepage}/>
+
+                    {/* LOGIN  */}
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Registration}/>
+
+
+                    <Redirect to="/"/>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

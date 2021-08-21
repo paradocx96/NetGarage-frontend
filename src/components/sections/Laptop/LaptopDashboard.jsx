825473442d39c87162d5b87ba13b8499c6ff24c8
paradocx96 <adilanga@gmail.com>
2021-08-21 14:54:10 +0530

    Laptop - Dashboard Init

---
+++ src/components/sections/Laptop/LaptopDashboard.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LaptopDashboard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LaptopDashboard;

f5ff6f8ce2b3a5d13150c15dcc12ef9eba608c44
DeclanChirush <hirushgimhan2016@gmail.com>
2021-08-21 18:57:41 +0530

    User Login part 1 done!

--- src/services/ServiceUser.jsx
+++ src/services/ServiceUser.jsx
@@ -15,6 +15,41 @@ class StudentService extends Component{
             password
         });
     }
+    getUserByID(){
+        return axios.get(API_STUDENT_BACKEND_URL+"getUser-id/{id}")
+    }
+    updateUserAccount(id,email,username,password){
+        return axios.post(API_STUDENT_BACKEND_URL+"update-account",{
+            id,
+            email,
+            username,
+            password
+        });
+    }
+
+    //TODO: Function for Student Login
+    login(username,password){
+        return axios.post(API_STUDENT_BACKEND_URL+"sign-in",{
+            username,
+            password
+        }).then(response =>{
+            console.log(response.data.username);
+            console.log(response.data.email);
+            if(response.data.accessToken){
+                sessionStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+    }
+    //TODO: Get current user
+    getCurrentUser() {
+        return JSON.parse(sessionStorage.getItem('user'));
+    }
+
+    //TODO: Remove current user
+    logout() {
+        sessionStorage.removeItem("user");
+    }
 }
 
 export default new StudentService();

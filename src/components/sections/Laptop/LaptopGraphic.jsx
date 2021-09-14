6311f4a5f61897d66f70a945c95b4184be91a6e7
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 09:51:00 +0530

    Laptop - Sub Categories Graphic Init

---
+++ src/components/sections/Laptop/LaptopGraphic.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class LaptopGraphic extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Laptop Graphic</h1>
+            </div>
+        );
+    }
+}
+
+export default LaptopGraphic;

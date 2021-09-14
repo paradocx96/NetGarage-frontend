37e6098c6ff381c2bffb35c5edec93c8086d7945
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 10:34:45 +0530

    Laptop - Sub Categories UI Updated

--- src/components/sections/Laptop/LaptopSubCategories.jsx
+++ src/components/sections/Laptop/LaptopSubCategories.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import LaptopBrand from "./LaptopBrand";
+import LaptopProcessor from "./LaptopProcessor";
+import LaptopOs from "./LaptopOS";
+import LaptopGraphic from "./LaptopGraphic";
+import NavigationBarDashboard from "../../layouts/Navigation/NavigationBarDashboard";
+import LaptopAddBodyWall from "../../layouts/Laptop/LaptopAddBodyWall";
 
 class LaptopSubCategories extends Component {
 
+    divBack = {
+        backgroundColor: '#212121',
+        color: '#ffffff',
+    }
+
     render() {
         return (
-            <div>
+            <div style={this.divBack}>
+                <NavigationBarDashboard/>
 
                 <h1>Laptop Sub Categories</h1>
 
+                <section>
+                    <LaptopBrand/>
+                </section>
+
+                <section>
+                    <LaptopProcessor/>
+                </section>
+
+                <section>
+                    <LaptopOs/>
+                </section>
+
+                <section>
+                    <LaptopGraphic/>
+                </section>
             </div>
         );
     }

40b4b875a8d5363eb3b9238b608aa3e076d267d1
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 13:44:02 +0530

    Laptop - Sub Categories OS Add Function Done

--- src/components/sections/Laptop/LaptopOS.jsx
+++ src/components/sections/Laptop/LaptopOS.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
+import {Button, Col, Container, Form, Row, Table} from "react-bootstrap";
 
 import ServiceLaptopOS from "../../../services/ServiceLaptopOS";
-import {Button, Container, Table} from "react-bootstrap";
 
 class LaptopOs extends Component {
 
@@ -13,9 +13,9 @@ class LaptopOs extends Component {
             getAllOS: []
         }
 
-        // this.onSubmit = this.onSubmit.bind(this);
-        // this.onReset = this.onReset.bind(this);
-        // this.onNameHandle = this.onNameHandle.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onReset = this.onReset.bind(this);
+        this.onNameHandle = this.onNameHandle.bind(this);
     }
 
     // TODO: Initializing default values
@@ -34,14 +34,61 @@ class LaptopOs extends Component {
             );
     }
 
+    // TODO: Assign Name to State variables
+    onNameHandle = (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    // TODO: Submit form values
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        let value = {
+            name: this.state.name,
+            user: 'Admin'
+        }
+
+        // TODO: Save value in database
+        await ServiceLaptopOS.postLaptopOS(value)
+            .then(response => response.data)
+            .then((data) => {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log(error.message);
+            });
+
+        this.onReset();
+        await this.componentDidMount();
+    }
+
+    // TODO: Reset form values
+    onReset = () => {
+        this.setState(() => this.initialState)
+    }
+
     render() {
         return (
             <div>
                 <Container>
                     <h1>Laptop OS</h1>
 
-                    <section>
-
+                    <section className={'pt-3 pb-3'}>
+                        <Form onSubmit={this.onSubmit.bind(this)} onReset={this.onReset.bind(this)}>
+                            <Form.Group as={Row} controlId="Name">
+                                <Col sm={4}>
+                                    <Form.Control placeholder="New OS Name"
+                                                  name="name"
+                                                  required
+                                                  value={this.state.name}
+                                                  onChange={this.onNameHandle.bind(this)}/>
+                                </Col>
+                                <Col>
+                                    <Button type="submit" className="btn-success">SAVE</Button>{'\u00A0'}
+                                    <Button type="reset" className="btn-danger">RESET</Button>{'\u00A0'}
+                                </Col>
+                            </Form.Group>
+                        </Form>
                     </section>
 
                     <section className={'pt-3 pb-3'}>

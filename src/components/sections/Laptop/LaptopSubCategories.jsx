d815b6b0bc3d0b16c7e47871952c4c58bc6ec956
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 09:39:57 +0530

    Laptop - Sub Categories Init

---
+++ src/components/sections/Laptop/LaptopSubCategories.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LaptopSubCategories extends Component {
+    render() {
+        return (
+            <div>
+
+                <h1>Laptop Sub Categories</h1>
+
+            </div>
+        );
+    }
+}
+
+export default LaptopSubCategories;

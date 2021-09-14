5a7235cdba95b8062a03f97b2fd006ec7449c2dc
Navinda Lankesh <adilanga@gmail.com>
2021-09-15 00:34:28 +0530

    Laptop - Laptop Edit Route Added

--- src/components/sections/Laptop/LaptopEdit.jsx
+++ src/components/sections/Laptop/LaptopEdit.jsx
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
 
+import ServiceLaptop from "../../../services/ServiceLaptop";
+
 class LaptopEdit extends Component {
+
+    // TODO: Initializing state values and functions
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            LaptopList: []
+        }
+    }
+
+    componentDidMount = async () => {
+        const {match: {params}} = this.props;
+        this.setState({
+            id: params.lid
+        });
+
+        await ServiceLaptop.getLaptopObjectById(params.lid)
+            .then(response => response.data)
+            .then((data) => {
+                console.log(data);
+                // this.setState({LaptopList: data});
+            }).catch(error =>
+                console.log(error.message)
+            );
+    }
 
     render() {
         return (

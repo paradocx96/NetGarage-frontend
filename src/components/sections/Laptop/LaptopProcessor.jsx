dd3b6eb795403ae8d68c0628ff9c43360599227b
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 09:49:02 +0530

    Laptop - Sub Categories Processor Init

---
+++ src/components/sections/Laptop/LaptopProcessor.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class LaptopProcessor extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Laptop Processor</h1>
+            </div>
+        );
+    }
+}
+
+export default LaptopProcessor;

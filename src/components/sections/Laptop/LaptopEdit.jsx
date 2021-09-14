ad08a12c38bf4904f047b12977e8e6dea605c937
Navinda Lankesh <adilanga@gmail.com>
2021-09-15 00:08:39 +0530

    Laptop - Laptop Edit Init

---
+++ src/components/sections/Laptop/LaptopEdit.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class LaptopEdit extends Component {
+
+    render() {
+        return (
+            <div>
+
+                <h3>Edit Laptop</h3>
+
+            </div>
+        );
+    }
+}
+
+export default LaptopEdit;

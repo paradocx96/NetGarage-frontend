b1e256b1139fa0b70059c9d9d865d7f4ff19bcf7
Navinda Lankesh <adilanga@gmail.com>
2021-09-14 12:28:08 +0530

    Laptop - Sub Categories Brand List View

--- src/components/sections/Laptop/LaptopBrand.jsx
+++ src/components/sections/Laptop/LaptopBrand.jsx
@@ -1,12 +1,91 @@
 import React, {Component} from 'react';
+import {Button, Container, Table} from "react-bootstrap";
+import {Link} from "react-router-dom";
+
+import ServiceLaptopBrand from "../../../services/ServiceLaptopBrand";
 
 class LaptopBrand extends Component {
+
+    // TODO: Initializing state values and functions
+    constructor(props) {
+        super(props);
+        this.state = this.initialState;
+        this.state = {
+            getAllBrand: []
+        }
+    }
+
+    // TODO: Initializing default values
+    initialState = {
+        name: '',
+        user: ''
+    }
+
+    componentDidMount = async () => {
+
+        // TODO: GET ALL LAPTOP BRAND
+        await ServiceLaptopBrand.getLaptopBrand()
+            .then(response => response.data)
+            .then((data) => {
+                console.log(data);
+                this.setState({getAllBrand: data});
+            }).catch(error =>
+                console.log(error.message)
+            );
+    }
 
     render() {
         return (
             <div>
-                <h1>Laptop Brand</h1>
+                <Container>
+                    <h1>Laptop Brand</h1>
 
+                    <section>
+
+                    </section>
+
+                    <section>
+                        <Table striped bordered hover variant="dark" size="sm">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>User</th>
+                                <th>Date & Time</th>
+                                <th> </th>
+                                <th> </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.getAllBrand.length === 0 ?
+                                    <tr>
+                                        <td>{'Data Not Available!'}</td>
+                                    </tr>
+                                    :
+                                    this.state.getAllBrand.map((item) => (
+                                        <tr key={item.id}>
+                                            <td>#</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.user}</td>
+                                            <td>{item.datetime}</td>
+                                            <td>
+                                                <Button
+                                                    // onClick={this.handleEdit.bind(this, item.id)}
+                                                        className="btn-primary">Edit</Button>
+                                            </td>
+                                            <td>
+                                                <Button
+                                                    // onClick={this.submitDelete.bind(this, item.id)}
+                                                        className="btn-danger">Delete</Button>
+                                            </td>
+                                        </tr>
+                                    ))
+                            }
+                            </tbody>
+                        </Table>
+                    </section>
+                </Container>
             </div>
         );
     }

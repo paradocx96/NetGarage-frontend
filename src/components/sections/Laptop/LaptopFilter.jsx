f32a1321b32d6a2f71e8196c48c98ca0ed25e248
Navinda Lankesh <adilanga@gmail.com>
2021-09-20 12:19:48 +0530

    Laptop - Laptop Filter Init

---
+++ src/components/sections/Laptop/LaptopFilter.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class LaptopFilter extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>
+                    Laptop Filter
+                </h3>
+            </div>
+        );
+    }
+}
+
+export default LaptopFilter;

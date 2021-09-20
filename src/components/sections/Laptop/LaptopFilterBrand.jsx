75b18861745e272f885ef4cca7d0e7a18626548f
Navinda Lankesh <adilanga@gmail.com>
2021-09-20 12:57:52 +0530

    Laptop - LaptopFilterBrand Init

---
+++ src/components/sections/Laptop/LaptopFilterBrand.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LaptopFilterBrand extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Laptop Find By Brand</h3>
+
+            </div>
+        );
+    }
+}
+
+export default LaptopFilterBrand;

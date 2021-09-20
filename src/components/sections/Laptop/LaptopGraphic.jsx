d7f9c54c5584a29449fb4cadb73443ccc993aa4d
Navinda Lankesh <adilanga@gmail.com>
2021-09-20 23:31:39 +0530

    Laptop - Graphic Delete Added

--- src/components/sections/Laptop/LaptopGraphic.jsx
+++ src/components/sections/Laptop/LaptopGraphic.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Button, Col, Container, Form, Row, Table} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {confirmAlert} from "react-confirm-alert";
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 import ServiceLaptopGraphic from "../../../services/ServiceLaptopGraphic";
 
@@ -17,6 +19,8 @@ class LaptopGraphic extends Component {
         this.onSubmit = this.onSubmit.bind(this);
         this.onReset = this.onReset.bind(this);
         this.onNameHandle = this.onNameHandle.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.submitDelete = this.submitDelete.bind(this);
     }
 
     // TODO: Initializing default values
@@ -67,6 +71,44 @@ class LaptopGraphic extends Component {
     onReset = () => {
         this.setState(() => this.initialState)
     }
+
+    // TODO: Function for Delete
+    handleDelete = async (id) => {
+        await ServiceLaptopGraphic.deleteLaptopGraphicById(id)
+            .then(response => response.data)
+            .then((data) => {
+                console.log(data)
+            }).catch(error => {
+                console.log(error.message);
+            });
+
+        await this.componentDidMount();
+    }
+
+    // TODO: Function for confirm delete operation
+    submitDelete = (id) => {
+        confirmAlert({
+            title: 'Confirm to delete!',
+            message: 'Are you sure to delete this Graphic Brand?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => {
+                        this.handleDelete(id);
+                        console.log('Delete Operation Proceed!');
+                    }
+                },
+                {
+                    label: 'No',
+                    onClick: () => {
+                        console.log('Delete Operation Canceled!');
+                    }
+                }
+            ],
+            closeOnEscape: true,
+            closeOnClickOutside: true
+        });
+    };
 
     render() {
         return (
@@ -124,7 +166,7 @@ class LaptopGraphic extends Component {
                                             </td>
                                             <td>
                                                 <Button
-                                                    // onClick={this.submitDelete.bind(this, item.id)}
+                                                    onClick={this.submitDelete.bind(this, item.id)}
                                                     className="btn-danger">Delete</Button>
                                             </td>
                                         </tr>

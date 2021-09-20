187fd43992af1e404b4f5e39f687ac3e118f2538
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-09-20 22:18:26 +0530

    Added cards to PhoneInternal.jsx

--- src/components/pages/PhoneInternal.jsx
+++ src/components/pages/PhoneInternal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavigationBarDashboard from "../layouts/Navigation/NavigationBarDashboard";
-import {Col, Row} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 class PhoneInternal extends React.Component{
@@ -22,36 +22,64 @@ class PhoneInternal extends React.Component{
 
                     <Row>
                         <Col>
-                            <h2>Chipsets</h2>
-                            <Link to={'/chipsets/viewAll'}> View All Chipsets</Link> <br />
-                            <Link to={'/chipsets/addChipset'}> Add Chipsets</Link> <br />
-                            <Link to={'/chipsets/deleteChipset'}> Delete Chipsets</Link> <br />
+                            <Card border={'primary'}>
+                                <Card.Body>
+                                    <h2>Chipsets</h2>
+                                    <Link to={'/chipsets/viewAll'}> View All Chipsets</Link> <br />
+                                    <Link to={'/chipsets/addChipset'}> Add Chipsets</Link> <br />
+                                    <Link to={'/chipsets/deleteChipset'}> Delete Chipsets</Link> <br />
+                                </Card.Body>
+                            </Card>
+
                         </Col>
 
                         <Col>
-                            <h2>Phone OS</h2>
-                            <Link to={'/phone/os/viewAll'}> View All OS</Link> <br />
-                            <Link to={'/phone/os/addOS'}> Add OS</Link> <br />
-                            <Link to={'/phone/os/deleteOS'}> Delete OS</Link> <br />
+                            <Card border={'warning'}>
+                                <Card.Body>
+
+                                    <h2>Phone OS</h2>
+                                    <Link to={'/phone/os/viewAll'}> View All OS</Link> <br />
+                                    <Link to={'/phone/os/addOS'}> Add OS</Link> <br />
+                                    <Link to={'/phone/os/deleteOS'}> Delete OS</Link> <br />
+                                </Card.Body>
+                            </Card>
                         </Col>
 
                         <Col>
-                            <h2>Phone Brands</h2>
-                            <Link to={'/phone/brand/addBrand'}> Add Brand</Link> <br />
-                            <Link to={'/phone/brand/viewAll'}> View All Brands</Link> <br />
-                            <Link to={'/phone/brand/deleteBrand'}> Delete Brands</Link> <br />
+                            <Card border={'danger'}>
+                                <Card.Body>
+
+                                    <h2>Phone Brands</h2>
+                                    <Link to={'/phone/brand/addBrand'}> Add Brand</Link> <br />
+                                    <Link to={'/phone/brand/viewAll'}> View All Brands</Link> <br />
+                                    <Link to={'/phone/brand/deleteBrand'}> Delete Brands</Link> <br />
+                                </Card.Body>
+                            </Card>
                         </Col>
 
                         <Col>
-                            <h2>Phones</h2>
-                            <Link to={'/phones/phoneActions'}> Phone Actions</Link> <br />
-                            {/*<Link to={'/phones/viewAllPhonesInternal'}> View All Phones</Link> <br />*/}
-                            <Link to={'/phones/viewAllPhonesInternal1'}> View All Phones </Link> <br />
-                            <Link to={'/phones/addPhone'}> Add Phone</Link> <br />
-                            <Link to={'/phones/deletePhones'}> Delete Phones</Link> <br />
-                            {/*<Link to={'/phones/filter/publishedBrandFilter'}> Phone Brand Filter Published</Link> <br />
+                            <Card border={'success'}>
+                                <Card.Body>
+
+                                    <h2>Phones</h2>
+                                    <Row>
+                                        <Col>
+
+                                            <Link to={'/phones/phoneActions'}> Phone Actions</Link> <br />
+                                            {/*<Link to={'/phones/viewAllPhonesInternal'}> View All Phones</Link> <br />*/}
+                                            <Link to={'/phones/viewAllPhonesInternal1'}> View All Phones </Link> <br />
+                                            <Link to={'/phones/addPhone'}> Add Phone</Link> <br />
+                                        </Col>
+                                        <Col>
+
+                                            <Link to={'/phones/deletePhones'}> Delete Phones</Link> <br />
+                                        </Col>
+                                    </Row>
+                                    {/*<Link to={'/phones/filter/publishedBrandFilter'}> Phone Brand Filter Published</Link> <br />
                             <Link to={'/phones/filter/publishedChipsetFilter'}> Phone Chipset Filter Published</Link> <br />
                             <Link to={'/phones/filter/publishedOsFilter'}> Phone OS Filter Published</Link> <br />*/}
+                                </Card.Body>
+                            </Card>
                         </Col>
                     </Row>
                 </div>

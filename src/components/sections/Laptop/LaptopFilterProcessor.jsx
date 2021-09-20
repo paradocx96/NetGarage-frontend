44a160593733239d5754fe976d0a8c1f55c4c71c
Navinda Lankesh <adilanga@gmail.com>
2021-09-20 13:00:11 +0530

    Laptop - LaptopFilterProcessor Init

---
+++ src/components/sections/Laptop/LaptopFilterProcessor.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class LaptopFilterProcessor extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Laptop Find By Processor</h3>
+            </div>
+        );
+    }
+}
+
+export default LaptopFilterProcessor;

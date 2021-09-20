2b39401a3599f5a1b66310b9c5a370a30ab7113b
Navinda Lankesh <adilanga@gmail.com>
2021-09-20 12:59:09 +0530

    Laptop - LaptopFilterRam Init

---
+++ src/components/sections/Laptop/LaptopFilterRam.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class LaptopFilterRam extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Laptop Find By RAM Capacity</h3>
+            </div>
+        );
+    }
+}
+
+export default LaptopFilterRam;

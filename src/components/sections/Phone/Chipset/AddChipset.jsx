c7f9dae871108e0bf1cd5d7466914cb410166072
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-09-30 21:51:10 +0530

    Added FooterAdmin to phone chipset section.

--- src/components/sections/Phone/Chipset/AddChipset.jsx
+++ src/components/sections/Phone/Chipset/AddChipset.jsx
@@ -8,6 +8,7 @@ import NavigationBarDashboard from "../../../layouts/Navigation/NavigationBarDas
 import CommonCheckAuth from "../../../../services/CommonCheckAuth";
 import ServiceUser from "../../../../services/ServiceUser";
 import {Redirect} from "react-router-dom";
+import FooterAdmin from "../../../layouts/Footer/FooterAdmin";
 
 class AddChipset extends React.Component{
     constructor(props) {
@@ -221,6 +222,8 @@ class AddChipset extends React.Component{
                 </Form>
 
             </div>
+                <br/> <br/> <br/>
+                <FooterAdmin/>
             </div>
         );
     }
--- src/components/sections/Phone/Chipset/DeleteChipset.jsx
+++ src/components/sections/Phone/Chipset/DeleteChipset.jsx
@@ -8,6 +8,7 @@ import NavigationBarDashboard from "../../../layouts/Navigation/NavigationBarDas
 import CommonCheckAuth from "../../../../services/CommonCheckAuth";
 import ServiceUser from "../../../../services/ServiceUser";
 import {Redirect} from "react-router-dom";
+import FooterAdmin from "../../../layouts/Footer/FooterAdmin";
 
 
 class DeleteChipset extends React.Component {
@@ -143,6 +144,9 @@ class DeleteChipset extends React.Component {
                         }
                         </tbody>
                     </Table>
+
+                    <br/> <br/> <br/>
+                    <FooterAdmin/>
 
                 </div>
             );

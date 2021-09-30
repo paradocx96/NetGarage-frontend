1c12f2784df3ba6d511067841667f0ed10d2d383
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-09-30 10:35:24 +0530

    Edited NavigationBar.jsx

--- src/components/layouts/Navigation/NavigationBar.jsx
+++ src/components/layouts/Navigation/NavigationBar.jsx
@@ -16,24 +16,24 @@ class NavigationBar extends Component {
             <div>
                 <Navbar collapseOnSelect expand="lg" style={this.backColor} variant="dark">
                     <Container>
-                        <Link to={'/'} className={'navbar-brand'}>HOME</Link>
+                        <Link to={'/'} className={'navbar-brand'}>Home</Link>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
 
                             <Nav className="me-auto">
-                                <NavDropdown title="PHONE" id="collasible-nav-dropdown">
+                                <NavDropdown title="Phone" id="collasible-nav-dropdown">
                                     {/*<Link to={'/phones/main'} className={'dropdown-item'}>Phones</Link>*/}
                                     <Link to={'/phones/filter/publishedFilterMain'} className={'dropdown-item'}>Phones Finder</Link>
                                     <Link to={'/phones/compare'} className={'dropdown-item'}>Compare Phones</Link>
                                     {/*<NavDropdown.Item href="/phones/chipsets/viewAll">Mobile Finder</NavDropdown.Item>*/}
                                 </NavDropdown>
 
-                                <NavDropdown title="LAPTOP" id="collasible-nav-dropdown">
+                                <NavDropdown title="Laptop" id="collasible-nav-dropdown">
                                     <Link to={'/laptops'} className={'dropdown-item'}>Laptop</Link>
                                     <Link to={'/laptops-finder'} className={'dropdown-item'}>Laptop Finder</Link>
                                 </NavDropdown>
 
-                                <Link to={'/contact'} className={'nav-link'} >CONTACT</Link>
+                                <Link to={'/contact'} className={'nav-link'} >Contact</Link>
                             </Nav>
 
                         </Navbar.Collapse>

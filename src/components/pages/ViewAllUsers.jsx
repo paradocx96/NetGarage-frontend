e6ddfb10ab30bfbd88332a368b62106eaf8b71b9
DeclanChirush <hirushgimhan2016@gmail.com>
2021-09-30 14:43:29 +0530

    User PDF report generator done!

---
+++ src/components/pages/ViewAllUsers.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ViewAllUsersSection from "../sections/UserPDFGenerator/ViewAllUsers";
+class ViewAllUsers extends Component {
+    render() {
+        return (
+            <div>
+                <ViewAllUsersSection/>
+            </div>
+        );
+    }
+}
+
+export default ViewAllUsers;
